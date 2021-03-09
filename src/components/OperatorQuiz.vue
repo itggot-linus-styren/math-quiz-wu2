a4a7dbdf6b6b9e0a27f42a83f4755bf6e536caa9
Linus Styrén <linus.styren@ga.ntig.se>
2021-03-09 01:12:59 +0100

    lägg till state för quiz

--- src/components/OperatorQuiz.vue
+++ src/components/OperatorQuiz.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <p>{{ operator }}</p>
+    <!-- Visa uttrycket genom att interpolera operanderna och operatorn -->
+    <p>{{ operandLeft }} {{ operator }} {{ operandRight }}</p>
     <button @click="$emit('clearOperator')">Back</button>
   </div>
 </template>
@@ -8,7 +9,17 @@
 <script>
 export default {
   name: "OperatorQuiz",
-  props: ["operator"]
+  props: ["operator"],
+  data() {
+    /* Variabler som används i quiz för att hålla koll på uttrycket,
+    svarsalternativen och det korrekta svaret. */
+    return {
+      operandLeft: null,
+      operandRight: null,
+      answers: [],
+      expectedAnswer: null
+    };
+  }
 };
 </script>
 
