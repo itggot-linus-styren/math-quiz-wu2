d27ee7ca6650d3d58c0aeb183900980c047db37c
Linus Styrén <linus.styren@ga.ntig.se>
2021-03-09 01:25:04 +0100

    slumpa två operander

--- src/components/OperatorQuiz.vue
+++ src/components/OperatorQuiz.vue
@@ -24,10 +24,16 @@ export default {
     startQuiz() {
       /* Quiz logik stegvis */
       /* 1. Slumpa två operander */
+      this.operandLeft = Math.random() * 13;
+      this.operandRight = Math.random() * 13;
+      
       /* 2. Beräkna korrekt svar */
       /* 3. Generera felaktiga svar */
       /* 4. Blanda svaren */
     }
+  },
+  created() {
+    this.startQuiz();
   }
 };
 </script>
